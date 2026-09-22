30b6e50379a7565d238e4f7f1a29f002bb269a7e
umlaufisadora <umlaufisadora@gmail.com>
2026-09-22 20:42:10 -0300

    feat: CrConfiguração de segurança

--- src/main/java/br/com/srobots_satlas/mini_s/auth/config/SecurityConfig.java
+++ src/main/java/br/com/srobots_satlas/mini_s/auth/config/SecurityConfig.java
@@ -1,14 +1,71 @@
 package br.com.srobots_satlas.mini_s.auth.config;
 
+import br.com.srobots_satlas.mini_s.auth.filter.UserAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig
 {
+
+    @Autowired
+    private UserAuthenticationFilter userAuthenticationFilter;
+
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
             "/users/login",
             "/users"
     };
+
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+            "/users/tests"
+    };
+
+    public static final String [] ENDPOINTS_USER = {
+            "/users/test/user"
+    };
+
+    public static final String [] ENDPOINTS_PROFESSOR = {
+            "/users/test/professor"
+    };
+
+    public static final String [] ENDPOINTS_ADMIN = {
+            "/users/test/admin"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
+    {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                        .requestMatchers(ENDPOINTS_USER).hasRole("USER")
+                        .requestMatchers(ENDPOINTS_PROFESSOR).hasRole("PROFESSOR")
+                        .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMIN"))
+                .addFilterBefore(userAuthenticationFilter, UserAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 }
